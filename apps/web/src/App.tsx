316038da2da0f4641821e0fa410011ba248649f5
Samuel Croteau <SCroteau94@gmail.com>
2026-05-12 23:54:47 -0400

    no bottom

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <div className="min-h-dvh bg-background text-foreground">
       <AppHeader />
-      <div className="mx-auto flex max-w-7xl flex-col px-6 pt-4 pb-8">
+      <div className="mx-auto flex max-w-7xl flex-col px-6 pt-4 ">
         <div className="grid grid-cols-1 gap-5 lg:grid-cols-3 lg:items-start lg:gap-5">
           <div className="flex flex-col gap-5">
             <MotorStatusBlocks />
